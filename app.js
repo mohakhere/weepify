a38b6ca2cc4a0af0a25e521aa2a11708334f9777
Mohak Sethi <107026265+mohakhere@users.noreply.github.com>
2023-12-25 23:55:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const apiRoutes = require('./routes/apiRoutes');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// routes and middleware
+// routes
+app.use('/api', apiRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
