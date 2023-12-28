1bbc3cc7ea8c99f11df28e1e8d6a1e98c78eb11a
Mohak Sethi <107026265+mohakhere@users.noreply.github.com>
2023-12-28 23:43:50 +0530

    Update items.py

--- items.py
+++ items.py
@@ -1,14 +1,13 @@
-# routers/items.py
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
 from models import Item
+from typing import Optional
 
 router = APIRouter()
 
-@router.get("/items/{item_id}")
-def read_item(item_id: int, query_param: str = None):
-    return {"item_id": item_id, "query_param": query_param}
-
-@router.post("/items/")
-def create_item(item: Item):
-    # Process the item and return a response
-    return {"item_name": item.name, "item_description": item.description}
+def get_db():
+    # database connection logic
+    db = get_database_connection()
+    try:
+        yield db
+    finally:
+        db.close()
