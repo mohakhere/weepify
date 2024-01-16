12b4624389902a2a6ee0b5c55a911d17a911d13e
Mohak Sethi <107026265+mohakhere@users.noreply.github.com>
2024-01-16 21:57:22 +0530

    Update apiRoutes.js

--- apiRoutes.js
+++ apiRoutes.js
@@ -2,5 +2,5 @@ const express = require('express');
 const router = express.Router();
 
 // API routes
-
+// Clear doc reps and add the file extensions with their updated variable names
 module.exports = router;
