3aa02d6d7bb71a9b4629332bedfe279dcf1cf882
Mohak Sethi <107026265+mohakhere@users.noreply.github.com>
2024-01-21 23:22:45 +0530

    Delete apiRoutes.js

--- apiRoutes.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// API routes
-// Clear doc reps and add the file extensions with their updated variable names
-module.exports = router;
